3782cdcad0b97b2df0b429e45413758db1713308
CSA100 <chaanan100@gmail.com>
2021-03-28 17:10:53 +0800

    updated styling

---
+++ dist/js/customScript.js
@@ -0,0 +1,5 @@
+// scroll down prompt arrow
+gsap.timeline({ repeat: -1})
+.from('#arrow_1', {opacity: 0, ease: 'Power2.easeInOut'})
+.from('#arrow_2', {opacity: 0, ease: 'Power2.easeInOut'})
+.from('#arrow_3', {opacity: 0, ease: 'Power2.easeInOut'})
